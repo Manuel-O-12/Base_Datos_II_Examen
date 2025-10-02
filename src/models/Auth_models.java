d9bc51c29c855fe1a8de9bae26b53f82b234dd11
Gabriel3465 <gabriel2_23@alu.uabcs.mx>
2025-10-01 21:06:40 -0700

    se configuro las credenciales de la BD

--- src/models/Auth_models.java
+++ src/models/Auth_models.java
@@ -1,5 +1,13 @@
 package models;
 
+import config.DBConfig;
+
 public class Auth_models {
+	
+	DBConfig credentials = new DBConfig();
+	
+	String url = credentials.getUrl();
+	String user = credentials.getUsername();
+	String password = credentials.getPassword();
 
 }
