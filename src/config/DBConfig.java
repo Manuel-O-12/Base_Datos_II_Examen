2d224aea6f0850877fe8311cf89ad60f25adef98
Gabriel3465 <gabriel2_23@alu.uabcs.mx>
2025-10-01 19:09:44 -0700

    credenciales de la base de datos

--- src/config/DBConfig.java
+++ src/config/DBConfig.java
@@ -1,5 +1,30 @@
 package config;
 
-public class DBConfig {
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
+public class DBConfig {
+    private Properties props = new Properties();
+
+    public DBConfig() {
+        try (FileInputStream fis = new FileInputStream("config/db.properties")) {
+            props.load(fis);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getUrl() {
+        return props.getProperty("db.url");
+    }
+
+    public String getUsername() {
+        return props.getProperty("db.username");
+    }
+
+    public String getPassword() {
+        return props.getProperty("db.password");
+    }
 }
+
